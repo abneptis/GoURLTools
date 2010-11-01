1746592dc94c12b4f165de26a295f13aeef50946
James D. Nurmi <james@abneptis.com>
2010-11-01 13:05:26 -0700

    Update godoc

--- escape.go
+++ escape.go
@@ -16,26 +16,32 @@ func Rfc3986Reserved(i byte)(out bool){
  return
 }
 
+// Returns the hex encoded (lower-case) value of byte.
 func HexEncodeLower(b byte)(out string){
   hb := []byte{'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'}
   out = string([]byte {hb[(b & 0xf0) >> 4], hb[(b & 0x0f)]})
   return
 }
 
+// Returns the hex encoded (upper-case) value of byte.
 func HexEncodeUpper(b byte)(out string){
   hb := []byte{'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}
   out = string([]byte {hb[(b & 0xf0) >> 4], hb[(b & 0x0f)]})
   return
 }
 
+// Returns the hex encoded (upper-case) value of byte with a % prepended.
 func PercentUpper(b byte)(out string){
   return "%" + HexEncodeUpper(b)
 }
 
+// Returns the hex encoded (lower-case) value of byte with a % prepended.
 func PercentLower(b byte)(out string){
   return "%" + HexEncodeLower(b)
 }
 
+// Escape the string as follows: if etf(b) returns true, use the value generated
+// by ef(b), else use b.
 func Escape(in string, etf func(byte)(bool), ef func(byte)(string))(out string){
   for i := range(in){
     if etf(in[i]) {
